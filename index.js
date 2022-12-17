195988f86ae3f5b6958c0f63f9ba1fb53c9a3f27
ProgVitorHCF <120839986+ProgVitorHCF@users.noreply.github.com>
2022-12-17 23:40:55 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,48 @@
+
+nota1 = 1.0
+nota2 = 6.0
+
+media = (nota1 + nota2) /2
+
+conceito = ""
+
+if (media <= 4.0)
+{
+  conceito = "Ruim"
+}
+
+else if (media <= 6.0)
+{
+  conceito = "Regular"
+}
+
+else if (media <= 8.0)
+{
+  conceito = "Bom"
+}
+else
+{
+  conceito = "Ótimo"
+}
+
+console.log("Sua média foi", media)
+console.log("O seu conceito foi", conceito)
+
+switch (conceito)
+{
+  case "Ruim":
+    console.log("Precisa Estudar Mais")
+      break;
+      
+  case "Regular":
+    console.log("Esta quase lá")
+      break;
+      
+  case "Bom":
+    console.log("Continue assim")
+      break;
+      
+  case "Ótimo":
+    console.log("Mandou Bem !")
+      break;
+}
